6a615cbba9c4a42056d024ce5daaa503673e73c5
remote007 <pmnrai2013@gmail.com>
2022-03-24 07:21:05 +0530

    Update mod.go

--- mod.go
+++ mod.go
@@ -11,4 +11,7 @@ func main(){
 	r.HandleFunc("/",getBlockchain).Methods("GET")
 	r.HandleFunc("/",writeBlock).Methods("POST")
 	r.HandleFunc("/new",newBook).Methods("POST")
+
+	log.Println("Listening on port 3000")
+	log.Fatal(http.ListenAndServe(3000,r))
 }
