70e5f10e119fb6b25f0138827bbc72091d44b82a
remote007 <pmnrai2013@gmail.com>
2022-03-25 14:50:25 +0530

    update

--- mod.go
+++ mod.go
@@ -3,24 +3,33 @@ package main
 import(
 	"log"
 	// "fmt"
+	"encoding/json"
 	"net/http"
  	"github.com/gorilla/mux"
 )
 
 type Block struct{
+	Pos 		int
+	Data		BookCheckout
+	Hash		string
+	Timestamp	string
+	PrevHash	string
 
 }
 
 type BookCheckout struct{
-
+	BookId 		 string   'json:"book_id"'
+	User         string   'json:"user"'
+	CheckoutDate string	  'json:"checkout_date"'
+	IsGenesis    bool     'json:"is_genesis"'
 }
 
 type Book struct{
-	ID string
-	Title string
-	Author string
-	PublishDate string
-	ISBN string
+	ID 			 string   'json:"id"'
+	Title		 string	  'json:"title"'	
+	Author		 string   'json:"author"'
+	PublishDate	 string   'json:"publish_date"'
+	ISBN		 string   'json:"isbn"'
 }
 
 type Blockchain struct{
@@ -29,6 +38,16 @@ type Blockchain struct{
 }
 
 var Blockchain *Blockchain
+
+func newBook(w http.ResponseWriter , r *http.Request){
+	var book Book
+	if err := json.NewDecoder(r.body).Decode(&book); err!=nil{
+		w.writeHeader(http.StatusInternalServerError);
+		log.Printf("could not create")
+	}
+
+
+}
 
 func main(){
 	r := mux.NewRouter()
